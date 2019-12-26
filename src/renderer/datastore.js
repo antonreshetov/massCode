65d898e30082ce475cb41d25ac49dfd65325cc80
Anton Reshetov <reshetov.art@gmail.com>
2019-12-26 15:00:08 +0300

    chore: separate dev & prod folders for datastore

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -2,17 +2,20 @@ import Store from 'nedb'
 import path from 'path'
 import { remote } from 'electron'
 
+const isDev = process.env.NODE_ENV === 'development'
+const folder = isDev ? 'MassCode/dev' : 'MassCode'
+
 export default {
   snippets: new Store({
     autoload: true,
-    filename: path.join(remote.app.getPath('home'), 'MassCode/snippets.db')
+    filename: path.join(remote.app.getPath('home'), `${folder}/snippets.db`)
   }),
   tags: new Store({
     autoload: true,
-    filename: path.join(remote.app.getPath('home'), 'MassCode/tags.db')
+    filename: path.join(remote.app.getPath('home'), `${folder}/tags.db`)
   }),
   masscode: new Store({
     autoload: true,
-    filename: path.join(remote.app.getPath('home'), 'MassCode/masscode.db')
+    filename: path.join(remote.app.getPath('home'), `${folder}/masscode.db`)
   })
 }
