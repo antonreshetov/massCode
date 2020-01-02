1e800de825a184deedd5e894a2426755080590a5
Anton Reshetov <reshetov.art@gmail.com>
2020-01-02 19:15:34 +0300

    feat(ui): add buttom

---
+++ src/renderer/components/uikit/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="app-button"
+    v-on="$listeners"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppButton',
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.app-button {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 var(--spacing-sm);
+  -webkit-user-select: none;
+  background-color: var(--color-contrast-lower);
+  border: 1px solid var(--color-border);
+  height: 32px;
+  + .app-button {
+    margin-left: var(--spacing-xs);
+  }
+}
+</style>
