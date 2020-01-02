b4796d540ad4b2610adafea4d1f661dc1de4f94f
Anton Reshetov <reshetov.art@gmail.com>
2020-01-02 20:19:10 +0300

    feat: implement datastore as class, add import, update & move methods

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -1,21 +1,58 @@
 import Store from 'nedb'
 import path from 'path'
 import { remote } from 'electron'
+import electronStore from '@@/store'
+import store from '@/store'
+import fs from 'fs-extra'
 
-const isDev = process.env.NODE_ENV === 'development'
-const folder = isDev ? 'MassCode/dev' : 'MassCode'
+class DataStore {
+  constructor () {
+    this._defaultPath = remote.app.getPath('home') + '/massCode'
+    this._storedPath = electronStore.get('storagePath')
+    this._path = this._storedPath || this._defaultPath
 
-export default {
-  snippets: new Store({
-    autoload: true,
-    filename: path.join(remote.app.getPath('home'), `${folder}/snippets.db`)
-  }),
-  tags: new Store({
-    autoload: true,
-    filename: path.join(remote.app.getPath('home'), `${folder}/tags.db`)
-  }),
-  masscode: new Store({
-    autoload: true,
-    filename: path.join(remote.app.getPath('home'), `${folder}/masscode.db`)
-  })
+    this.init()
+  }
+
+  init () {
+    this.snippets = new Store({
+      autoload: true,
+      filename: path.join(this._path, '/snippets.db')
+    })
+    this.tags = new Store({
+      autoload: true,
+      filename: path.join(this._path, '/tags.db')
+    })
+    this.masscode = new Store({
+      autoload: true,
+      filename: path.join(this._path, '/masscode.db')
+    })
+
+    store.commit('app/SET_STORAGE_PATH', this._path)
+  }
+
+  updatePath () {
+    this._storedPath = electronStore.get('storagePath')
+    this._path = this._storedPath || this._defaultPath
+    this.init()
+  }
+
+  import (from) {
+    electronStore.set('storagePath', from)
+    this.updatePath()
+  }
+
+  async move (to) {
+    try {
+      await fs.move(this._path, to, { overwrite: true })
+      electronStore.set('storagePath', to)
+      this.updatePath()
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
+
+const db = new DataStore()
+
+export default db
