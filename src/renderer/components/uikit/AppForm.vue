ae5819558782b901401bd7a21bb35195f6276d5c
Anton Reshetov <reshetov.art@gmail.com>
2020-01-02 19:18:46 +0300

    feat(ui): add form

---
+++ src/renderer/components/uikit/AppForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-form">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppForm',
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss"></style>
---
+++ src/renderer/components/uikit/AppFormItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app-form-item">
+    <div
+      class="app-form-item__label"
+      :style="{
+        width: labelWidth + 'px'
+      }"
+    >
+      {{ label }}
+    </div>
+    <div class="app-form-item__body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFormItem',
+
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    labelWidth: {
+      type: Number,
+      default: 120
+    }
+  },
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss">
+.app-form-item {
+  display: flex;
+  + .app-form-item {
+    margin-top: var(--spacing-sm);
+  }
+  &__label {
+    margin-right: var(--spacing-sm);
+  }
+  &__body {
+    flex-grow: 1;
+  }
+}
+</style>
