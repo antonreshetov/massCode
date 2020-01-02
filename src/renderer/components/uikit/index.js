653ee72a083b75c5e8c232ec524c649459f20dd3
Anton Reshetov <reshetov.art@gmail.com>
2020-01-02 19:19:25 +0300

    chore: update uikit index

--- src/renderer/components/uikit/index.js
+++ src/renderer/components/uikit/index.js
@@ -1,7 +1,22 @@
 import AppIcon from './AppIcon.vue'
 import AppInput from './AppInput.vue'
+import AppMenu from './AppMenu.vue'
+import AppMenuItem from './AppMenuItem.vue'
+import AppButton from './AppButton.vue'
+import AppForm from './AppForm.vue'
+import AppFormItem from './AppFormItem.vue'
+import AppSelect from './AppSelect.vue'
 
-const components = [AppIcon, AppInput]
+const components = [
+  AppIcon,
+  AppInput,
+  AppMenu,
+  AppMenuItem,
+  AppButton,
+  AppForm,
+  AppFormItem,
+  AppSelect
+]
 
 export default {
   install (Vue, options = {}) {
