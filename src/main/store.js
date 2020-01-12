98541b4a0f8914dd04e5a983860afa8284716f85
Anton Reshetov <reshetov.art@gmail.com>
2020-01-12 16:35:02 +0300

    refactor(main): store with scheme

--- src/main/store.js
+++ src/main/store.js
@@ -1,3 +1,74 @@
 const Store = require('electron-store')
 
-export default new Store()
+const store = new Store({
+  schema: {
+    bounds: {
+      type: 'object',
+      default: null
+    },
+    sidebarWidth: {
+      type: 'number',
+      default: null
+    },
+    snippetListWidth: {
+      type: 'number',
+      default: null
+    },
+    selectedFolderId: {
+      type: 'string',
+      default: null
+    },
+    selectedFolderIds: {
+      type: 'string',
+      default: null
+    },
+    selectedSnippetId: {
+      type: 'string',
+      default: null
+    },
+    snippetsSort: {
+      type: 'string',
+      default: 'updateAt'
+    },
+    storagePath: {
+      type: 'string',
+      default: 'updateAt'
+    },
+    theme: {
+      type: 'string',
+      default: 'dark'
+    },
+    allowAnalytics: {
+      type: 'boolean',
+      default: true
+    },
+    install: {
+      type: 'string',
+      default: null
+    },
+    preferences: {
+      type: 'object',
+      properties: {
+        assistant: {
+          type: 'object',
+          properties: {
+            enable: {
+              type: 'boolean',
+              default: true
+            },
+            shortcut: {
+              type: 'string',
+              default: 'Option+S'
+            }
+          }
+        }
+      }
+    }
+  }
+})
+
+if (!store.get('preferences')) {
+  store.set('preferences', { assistant: {} })
+}
+
+export default store
