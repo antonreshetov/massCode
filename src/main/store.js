5e52920b0dd21a0b7a795d3108c9fcab27cc0a7d
Anton Reshetov <reshetov.art@gmail.com>
2020-01-13 18:12:49 +0300

    fix(main): remove store shema for exist fields

--- src/main/store.js
+++ src/main/store.js
@@ -2,50 +2,6 @@ const Store = require('electron-store')
 
 const store = new Store({
   schema: {
-    bounds: {
-      type: 'object',
-      default: null
-    },
-    sidebarWidth: {
-      type: 'number',
-      default: null
-    },
-    snippetListWidth: {
-      type: 'number',
-      default: null
-    },
-    selectedFolderId: {
-      type: 'string',
-      default: null
-    },
-    selectedFolderIds: {
-      type: 'string',
-      default: null
-    },
-    selectedSnippetId: {
-      type: 'string',
-      default: null
-    },
-    snippetsSort: {
-      type: 'string',
-      default: 'updateAt'
-    },
-    storagePath: {
-      type: 'string',
-      default: 'updateAt'
-    },
-    theme: {
-      type: 'string',
-      default: 'dark'
-    },
-    allowAnalytics: {
-      type: 'boolean',
-      default: true
-    },
-    install: {
-      type: 'string',
-      default: null
-    },
     preferences: {
       type: 'object',
       properties: {
