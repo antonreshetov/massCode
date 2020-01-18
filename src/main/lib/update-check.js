e3256f5e601b5c27b081dbdba21b42130837b767
Anton Reshetov <reshetov.art@gmail.com>
2020-01-18 07:31:09 +0300

    refactor(main): check for update

--- src/main/lib/update-check.js
+++ src/main/lib/update-check.js
@@ -1,18 +1,8 @@
 import axios from 'axios'
-import { config } from 'dotenv'
 import { mainWindow } from '../main'
-
-config()
 
 const pkg = require('../../../package.json')
 const isDev = process.env.NODE_ENV === 'development'
-
-const gitHubApi = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: {
-    Authorization: `token ${process.env.GITHUB_TOKEN}`
-  }
-})
 
 async function checkForUpdatesAndNotify () {
   if (isDev) return
@@ -20,12 +10,16 @@ async function checkForUpdatesAndNotify () {
   const currentVersion = pkg.version
 
   setInterval(async () => {
-    const res = await gitHubApi.get('/repos/antonreshetov/massCode/tags')
+    const res = await axios.get(
+      'https://github.com/antonreshetov/masscode/releases/latest'
+    )
 
-    if (res.data && res.data[0]) {
-      const tagNumber = res.data[0].name.substring(1)
-
-      if (tagNumber !== currentVersion) {
+    if (res) {
+      const latest = res.request.socket._httpMessage.path
+        .split('/')
+        .pop()
+        .substring(1)
+      if (latest !== currentVersion) {
         mainWindow.webContents.send('update-available')
       }
     }
